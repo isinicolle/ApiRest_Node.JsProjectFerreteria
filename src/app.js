83216d57f843c17435b3fb145b4093273b6e8557
Isis Zapata <isin_zapataf@unicah.edu>
2022-02-16 21:56:13 -0600

    Update app.js
    iniciando puerto 6001 la app

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+
+//const rutas = require('./rutas');
+const app = express(); 
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+
+app.use(express.json());
+
+app.set('json spaces',2);
+
+//app.use('/api/', rutas );
+
+//definir el puerto que se usara en el servidor
+app.listen(6001, ()=>
+{
+    console.log("Servidor iniciado en el puerto 6001");
+});
