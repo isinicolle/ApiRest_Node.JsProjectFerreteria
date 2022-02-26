cf399138954e0fd8057f14556672aeffa0f7c2c8
Javier C <acej2207@gmail.com>
2022-02-26 13:32:45 -0600

    todo crud

---
+++ src/controladores/controladorCategoria.js
@@ -0,0 +1,90 @@
+const {PrismaClient} = require('@prisma/client') ;
+const prisma = new PrismaClient();
+const ModeloCategoria = prisma.categorias;
+
+exports.listarCategoria = async(req, res)=>{
+    const listarCategoria = await ModeloCategoria.findAll();
+    if(listarCategoria.length == 0){
+        res.send("No existen datos");
+    }else{
+        res.json(listarCategoria);
+    }
+}
+
+exports.guardarCategoria = async (req, res) => {
+    try {
+        const Categoria = await prisma.categorias.create({
+            data: req.body,
+        })
+        res.json(Categoria);
+    } catch (error) {
+        console.log(error)
+        next(error);
+    }
+};
+
+exports.eliminarCategoria = async (req, res) => {
+    const {id} = req.query;
+    if(!id) {
+        res.send("Envie el id de registro");
+    }
+    else {
+        try {
+            const eliminarCategoria = await prisma.categorias.delete(
+                {
+                    where: {
+                        id_categoria: Number(id),
+                    }
+                }
+            )
+            res.json(eliminarCategoria)
+        } catch (error) {
+            next(error)
+        }
+        }
+}
+
+exports.buscarCategoria = async (req, res) => {
+    const {id} = req.query;
+    if(!id) {
+        res.send("Envie el id de registro");
+    }
+    else {
+        try {
+            const buscarCategoria = await prisma.categorias.findUnique(
+                {
+                    where: {
+                        id_categoria: Number(id),
+                    }
+                }
+            )
+            res.json(buscarCategoria)
+        } catch (error) {
+            next(error)
+        }
+        }
+}
+
+exports.ModificarCategoria = async (req, res) => {
+    
+    try {
+        const {id_categoria} =req.query;
+        const {descripcion_categoria, estado} = req.body;
+        const categoria = await prisma.categorias.update({
+        where:
+        {
+            id_categoria: id_categoria,
+        },
+        data: 
+        {
+            descripcion_categoria:descripcion_categoria,
+            estado: estado,
+        }
+        
+        })
+        res.json(categoria);
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+}
---
+++ src/controladores/controladorCiudad.js
@@ -0,0 +1,92 @@
+const {PrismaClient} = require('@prisma/client') ;
+const prisma = new PrismaClient();
+const ModeloCiudad = prisma.ciudades;
+
+exports.listarCiudad = async(req, res)=>{
+    const listarCiudad = await ModeloCiudad.findAll();
+    if(listarCiudad.length == 0){
+        res.send("No existen datos");
+    }else{
+        res.json(listarCiudad);
+    }
+}
+
+exports.guardarCiudad = async (req, res) => {
+    try {
+        const Ciudad = await prisma.ciudades.create({
+            data: req.body,
+        })
+        res.json(Ciudad);
+    } catch (error) {
+        console.log(error)
+        next(error);
+    }
+};
+
+exports.eliminarCiudad = async (req, res) => {
+    const {id} = req.query;
+    if(!id) {
+        res.send("Envie el id de registro");
+    }
+    else {
+        try {
+            const eliminarCiudad = await prisma.ciudades.delete(
+                {
+                    where: {
+                        id_ciudad: Number(id),
+                    }
+                }
+            )
+            res.json(eliminarCiudad)
+        } catch (error) {
+            next(error)
+        }
+        }
+}
+
+exports.buscarCiudad = async (req, res) => {
+    const {id} = req.query;
+    if(!id) {
+        res.send("Envie el id de registro");
+    }
+    else {
+        try {
+            const buscarCiudad = await prisma.ciudades.findUnique(
+                {
+                    where: {
+                        id_ciudad: Number(id),
+                    }
+                }
+            )
+            res.json(buscarCiudad)
+        } catch (error) {
+            next(error)
+        }
+        }
+}
+
+exports.ModificarCiudad = async (req, res) => {
+    
+    try {
+        const {id_ciudad} =req.query;
+        const {descripcion_ciudad, estado, codigoPostal, id_departamento} = req.body;
+        const ciudad = await prisma.ciudades.update({
+        where:
+        {
+            id_ciudad:id_ciudad
+        },
+        data: 
+        {
+            nombre_ciudad: descripcion_ciudad,
+            estado: estado,
+            codigoPostal:codigoPostal,
+            id_departamento: id_departamento,
+        }
+        
+        })
+        res.json(ciudad);
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+}
