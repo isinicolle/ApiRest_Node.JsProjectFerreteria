3dc1525a4106794aa3c94530aac37b6735a0dfa6
Isis Zapata <isin_zapataf@unicah.edu>
2022-02-28 13:17:11 -0600

    avanzan2
    avance sin probar quizas isve quizas no quien sabe

--- src/configuraciones/emailer.js
+++ src/configuraciones/emailer.js
@@ -9,22 +9,22 @@ var transport = nodemailer.createTransport({
     }
   });
 
- const sendMail = async () => 
+ const sendMail = async (correo) => 
  {
    const transporter = nodemailer.createTransport()
    const info = await transport.sendMail(
     {
         from: '"Fred Foo 👻" <foo@example.com>', // sender address
-        to: "bar@example.com, baz@example.com", 
+        to: `${correo}`, 
         subject: "Ferreteria Ferretear",
         html: `   <div style="max-width: 700px; margin:auto; border: 10px solid #ddd;">
         <h2 style="text-align: center; text-transform: uppercase; color: teal;">GRACIAS POR PREFERIR NUESTRO SERVICIO</h2>
     <p>Se a creado exitosamente su cuenta!</p>    
-    <p>Se a creado exitosamente su cuenta! haga click para validarlo</p>    
+    <p>Puedes iniciar sesion en tu cuenta no requiere de una acivacion previa</p>    
     </div>`
     
     }
    )
  }
 
- exports.sendMail = () => sendMail()
+ exports.sendMail = (correo) => sendMail(correo)
