d5e0200d02cfd8f8cf8b74ebfc0ff3a3391617f4
Isis Zapata <isin_zapataf@unicah.edu>
2022-02-27 23:25:24 -0600

    Parte del login
    Uniendo la parte de Nallely

--- src/rutas/rutaUsuarioCliente.js
+++ src/rutas/rutaUsuarioCliente.js
@@ -5,9 +5,13 @@ const router = Router();
 //trabajando con rutas
 router.get('/listarUsuarioCliente', controladorUsuarioCliente.listarUsuarioCliente);
 router.get('/buscarUsuarioCliente', controladorUsuarioCliente.buscarUsuarioCliente);
+
 router.post('/insertarUsuarioCliente', controladorUsuarioCliente.insertarUsuariocliente);
 router.delete('/eliminarUsuarioCliente', controladorUsuarioCliente.eliminarUsuariocliente);
 router.put('/actualizarUsuarioCliente', controladorUsuarioCliente.actualizarCliente);
+
 router.put('/actualizarUsuarioEstadoCliente', controladorUsuarioCliente.actualizarEstadoCliente);
 router.put('/recoveryclave', controladorUsuarioCliente.recuperarContrasena);
+router.get('/loginUsuarioCliente', controladorUsuarioCliente.loginUsuarioCliente);
+
 module.exports=router; 
