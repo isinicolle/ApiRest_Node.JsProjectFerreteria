27117cd9ebae8536baa4e598d1cd9e01305da37f
Isis Zapata <isin_zapataf@unicah.edu>
2022-02-28 13:52:04 -0600

    Update emailer.js
    correito

--- src/configuraciones/emailer.js
+++ src/configuraciones/emailer.js
@@ -1,4 +1,9 @@
 const nodemailer = require('nodemailer')
+const htmlt = `   <div style="max-width: 700px; margin:auto; border: 10px solid #ddd;">
+<h2 style="text-align: center; text-transform: uppercase; color: teal;">GRACIAS POR PREFERIR NUESTRO SERVICIO</h2>
+<p>Se a creado exitosamente su cuenta!</p>    
+<p>Puedes iniciar sesion en tu cuenta no requiere de una acivacion previa</p>    
+</div>`
 
 var transport = nodemailer.createTransport({
     host: "smtp.mailtrap.io",
@@ -11,20 +16,19 @@ var transport = nodemailer.createTransport({
 
  const sendMail = async (correo) => 
  {
-   const transporter = nodemailer.createTransport()
    const info = await transport.sendMail(
     {
         from: '"Fred Foo 👻" <foo@example.com>', // sender address
         to: `${correo}`, 
         subject: "Ferreteria Ferretear",
-        html: `   <div style="max-width: 700px; margin:auto; border: 10px solid #ddd;">
-        <h2 style="text-align: center; text-transform: uppercase; color: teal;">GRACIAS POR PREFERIR NUESTRO SERVICIO</h2>
-    <p>Se a creado exitosamente su cuenta!</p>    
-    <p>Puedes iniciar sesion en tu cuenta no requiere de una acivacion previa</p>    
-    </div>`
+        html: htmlt,
     
     }
-   )
+   );
+
+   console.log("Message sent: %s " , info.messageId)
+
+   return
  }
 
  exports.sendMail = (correo) => sendMail(correo)
