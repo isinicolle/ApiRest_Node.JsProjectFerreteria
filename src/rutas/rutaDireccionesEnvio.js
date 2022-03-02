5f0cb1c365cd0b8637afddac19faae5dcbce3bc2
Nallely Reyes <na_reyesd@unicah.edu>
2022-03-02 01:03:14 -0600

    Funcion de direccion por usuario

--- src/rutas/rutaDireccionesEnvio.js
+++ src/rutas/rutaDireccionesEnvio.js
@@ -5,6 +5,7 @@ const router = Router();
 //trabajando con rutas
 router.get('/listarDireccionesEnvio', controladorDireccionesEnvio.listarDireccionesEnvio);
 router.get('/buscarDireccionesEnvio', controladorDireccionesEnvio.buscarDireccionEnvio);
+router.get('/direccionEnvioXUsuario', controladorDireccionesEnvio.DireccionEnvioXUsuario);
 router.post('/insertarDireccionesEnvio', controladorDireccionesEnvio.insertarDireccionenvio);
 router.delete('/eliminarDireccionesEnvio', controladorDireccionesEnvio.eliminarDireccionEnvio);
 router.put('/actualizarDireccionEnvio', controladorDireccionesEnvio.actualizarDireccionEnvio);
